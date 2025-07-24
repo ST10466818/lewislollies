4286153497cdb8fae6e74ed7c59cc73e4a4b9c94
https://github.com/ST10466818/packabletest.git <st10466818@vcconnect.co.za>
2025-07-24 11:22:32 +0200

    adjusted text style

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Welcome to your first proper React Native App!</Text>
+      <Text>Enter your name here:</Text>
+      <TextInput placeholder='First name'></TextInput>
+      <Text>Enter your surname here:</Text>
+      <TextInput placeholder='Surname'></TextInput>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  newText:{
+    fontSize:22,
+    fontFamily:'Monaco', 
+    marginVertical:18,
+    textAlign:'center',
+    fontStyle:'italic',
+    
+  }
 });

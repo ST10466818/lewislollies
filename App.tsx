e58990ddd46b128ee4ff5c5dd36433a442641069
https://github.com/ST10466818/packabletest.git <st10466818@vcconnect.co.za>
2025-07-29 08:47:01 +0200

    updating

--- App.tsx
+++ App.tsx
@@ -1,21 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { Alert, Button, StyleSheet, Text, TextInput, View , Image} from 'react-native';
 
 export default function App() {
+  const[Name, setName]= useState('');
+  const[Surname, setSurname]= useState('');
+
+  console.log("Starting App");
   return (
     <View style={styles.container}>
       <Text style={styles.newText}>Welcome to your first proper React Native App!</Text>
       <Text style={styles.newText}>Enter your name here:</Text>
+      <Text style={styles.HeadingText}>Enter Name:</Text>
+      <TextInput style={styles.InputBoxs}placeholder='First Name'
+      onChangeText={newText => setName(newText)}/>
+      <TextInput style={styles.InputBoxs}placeholder='Surname'
+      onChangeText={newText => setSurname(newText)}/>
       <TextInput placeholder='First name'></TextInput>
       <Text style={styles.newText}>Enter your surname here:</Text>
       <TextInput placeholder='Surname'></TextInput>
       <StatusBar style="auto" />
       <Button
-      title='Press here'
+      title='Add User'
       color="#f194ff"
       onPress={() => Alert.alert('Simple Button pressed')}/>
       <Image source={require("./assets/imgone.jpg")}
         style={styles.image}/>
+        console.log("Name:" + Name +
+        "Surname:"+ Surname)
     </View>
   );
 }
@@ -42,5 +54,11 @@ const styles = StyleSheet.create({
   image:{
     width: 150,
     height: 150,
-  }
+  },
+
+  InputFlex:{
+    flexDirection:'row',
+    marginTop:30,
+    justifyContent:'space-evenly',
+  },
 });
